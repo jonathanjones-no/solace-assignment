b7e7448bd3411e767c5522b08891dc267acb8cbc
jonathanjones.no <jonathanjones.no@gmail.com>
2025-04-17 22:20:33 -0600

    Adds and applies Stacks styling
    Adds @stackoverflow/stacks NPM package and applies styling to the table page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,50 +42,62 @@ export default function Home() {
   };
 
   return (
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
-      <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term"></span>
-        </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
+    <main>
+      <div className="s-page-title bs-lg">
+        <h1 className="s-page-title--header mt16 ml16">
+          Solace Advocates
+        </h1>
       </div>
-      <br />
-      <br />
-      <table>
-        <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
-        </thead>
-        <tbody>
-          {filteredAdvocates.map((advocate) => {
-            return (
+      <div className="mx-auto w100 wmx12">
+        <div className="d-flex gy4 fd-column">
+          <div className="flex--item mt16"> 
+            <label className="s-label" htmlFor="search-term">Search</label>
+          </div>
+          <div className="flex--item">
+            <p className="s-description mtn2 mb0">What are you looking for?</p>
+          </div>
+          <div className="flex--item mb2 bs-md">
+            <input className="s-input" id="search-term" type="text" onChange={onChange} />
+          </div>
+          <div className="flex--item mb16">
+            <button className="flex--item s-btn s-btn__outlined bs-md" onClick={onClick}>Reset Search</button>
+          </div>
+        </div>
+        <div className="s-table-container bs-lg mb16">
+          <table className="s-table s-table__stripes s-table__sm">
+            <thead>
               <tr>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
-                <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
-                  ))}
-                </td>
-                <td>{advocate.yearsOfExperience}</td>
-                <td>{advocate.phoneNumber}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">City</th>
+                <th scope="col">Degree</th>
+                <th scope="col">Specialties</th>
+                <th scope="col">Years of Experience</th>
+                <th scope="col">Phone Number</th>
+             </tr>
+            </thead>
+            <tbody>
+              {filteredAdvocates.map((advocate, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{advocate.firstName}</td>
+                    <td>{advocate.lastName}</td>
+                    <td>{advocate.city}</td>
+                    <td>{advocate.degree}</td>
+                    <td>
+                      {advocate.specialties.map((s) => (
+                        <div key={s}>{s}</div>
+                      ))}
+                    </td>
+                    <td>{advocate.yearsOfExperience}</td>
+                    <td>{advocate.phoneNumber}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </main>
   );
 }
